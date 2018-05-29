972b050d51e0eb36abdd49281c8c23d96946c7e9
MaLei <826908021@qq.com>
2018-05-29 18:09:18 +0800

    爬取豆瓣读书标签包含书籍信息 5.29 18:06 公司更新

--- 豆瓣读书爬虫/douban.py
+++ 豆瓣读书爬虫/douban.py
@@ -30,7 +30,7 @@ connection.commit()
 #     # ignore_discard的意思是即使cookies将被丢弃也将它保存下来；
 #     # ignore_expires的意思是如果在该文件中cookies已经存在，则覆盖原文件写入
 #     cookie.save(ignore_discard=True,ignore_expires=True)
-requests
+
 
 
 def login(url):
