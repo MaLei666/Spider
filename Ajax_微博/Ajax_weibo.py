2ece70aa6c0c130ab6577dad6593c1f0d2d1c344
MaLei <826908021@qq.com>
2018-06-01 17:45:58 +0800

    Ajax技术爬取新浪微博博主所有发表微博 5.29 18:06 公司更新

--- Ajax_微博/Ajax_weibo.py
+++ Ajax_微博/Ajax_weibo.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 from urllib.parse import urlencode
 import requests,pymysql
 from pyquery import PyQuery as pq
+from selenium import webdriver
+from time import sleep
 
 # 连接数据库
 connection = pymysql.connect(host='localhost',
@@ -11,31 +14,10 @@ connection = pymysql.connect(host='localhost',
                              db='python',
                              charset='utf8')
 
-cursor=connection.cursor()
+cursor = connection.cursor()
 sql = "USE python;"
 cursor.execute(sql)
 connection.commit()
-
-
-weibo = '''
-    CREATE TABLE weibo(
-        序号 INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-        ID VARCHAR (255),
-        text VARCHAR (255),
-        attitudes VARCHAR (255),
-        comments VARCHAR (255), 
-        reposts VARCHAR (255) 
-    )
-'''
-
-
-try:
-    cursor.execute(weibo)
-    connection.commit()
-except:
-    pass
-# a= '''ALTER TABLE `python`.`weibo` CHANGE COLUMN `序号` `序号` INT NOT NULL AUTO_INCREMENT  ;'''
-# cursor.execute(a)
 
 base_url='https://m.weibo.cn/api/container/getIndex?'
 headers = {
@@ -44,6 +26,59 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
     'X-Requested-With': 'XMLHttpRequest',
 }
+
+def create_sheet(bozhu):
+    try:
+        weibo = '''
+            CREATE TABLE weibo(
+                ID  VARCHAR (255) NOT NULL PRIMARY KEY,
+                text VARCHAR (255),
+                attitudes VARCHAR (255),
+                comments VARCHAR (255), 
+                reposts VARCHAR (255) 
+            )
+        '''
+        # 序号 INT  NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        cursor.execute(weibo)
+        connection.commit()
+    except:
+        pass
+
+def url_get():
+    # # 自动保持cookie,不需要自己维护cookie内容
+    # cookies = {}
+    # s = requests.session()
+    # with open('E:\example\豆瓣读书爬虫\cookie.txt')as file:
+    #     raw_cookies = file.read()
+    #     for line in raw_cookies.split(';'):
+    #         key, value = line.split('=', 1)
+    #         cookies[key] = value
+    #         # 完善header
+    # header = {'Upgrade-Insecure-Requests': '1',
+    #           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0',
+    #           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    #           'Accept-Encoding': 'gzip, deflate, br',
+    #           'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+    #           }
+    # # get请求，应答解码
+    # response = s.get(url=xl_url, headers=header,cookies=cookies)
+    browser=webdriver.PhantomJS()
+    browser.get(url='https://m.weibo.cn/')
+    wb_name=browser.find_element_by_class_name("W_input")
+    wb_name.send_keys(input('输入博主ID：'))
+    sleep(10)
+    search=browser.find_element_by_class_name('W_ficon ficon_search S_ficon')
+    search.click()
+    sleep(5)
+    bz_num=browser.find_element_by_class_name('name_txt')
+    bz_num.click()
+    sleep(5)
+    # 开启了一个新页面，需要跳转到新页面
+    handles = browser.window_handles
+    browser.switch_to_window(handles[1])
+
+
+
 
 # https://m.weibo.cn/api/container/getIndex?type=uid&value=2145291155&containerid=1076032145291155
 # 拼接url
@@ -65,10 +100,11 @@ def get_page(page):
     except requests.ConnectionError as e:
         print('Error', e.args)
 
+
+# 存储数据，存储到数据库
 def parse_page(json):
     if json:
         items = json.get('data').get('cards')
-        # print(items)
         for index,item in enumerate(items):
             if page == 1 and index == 1:
                 continue
@@ -80,23 +116,22 @@ def parse_page(json):
                 # weibo['attitudes'] = item.get('attitudes_count')
                 # weibo['comments'] = item.get('comments_count')
                 # weibo['reposts'] = item.get('reposts_count')
-
                 weibo = []
                 weibo.append(item.get('id'))
-                text=pq(item.get('text')).text()
-
-                weibo.append()
-                # weibo.append('无内容')
+                weibo.append(pq(item.get('text')).text())
                 weibo.append(item.get('attitudes_count'))
                 weibo.append(item.get('comments_count'))
                 weibo.append(item.get('reposts_count'))
-
-                sql = '''INSERT IGNORE INTO weibo (ID,text,attitudes,comments,reposts)
-                      VALUES (%s,%s,%s,%s,%s) on duplicate key update ID=values(ID)'''
-
-                cursor.execute(sql,weibo)
-                connection.commit()
+                # 遇见重复数据，pass，是根据主键来判断，如果是重复数据，忽略，但是报警告
+                try:
+                    sql = '''INSERT INTO weibo (ID,text,attitudes,comments,reposts)
+                          VALUES (%s,%s,%s,%s,%s) '''
+                    cursor.execute(sql, weibo)
+                    connection.commit()
+                except:
+                    pass
             yield weibo
+
 
 if __name__ == '__main__':
     for page in range(1, 17):
@@ -104,4 +139,11 @@ if __name__ == '__main__':
         results = parse_page(json)
         for result in results:
             print(result)
+
+
+
+
 cursor.close()
+
+# 可以爬任意指定博主所有微博，以博主名建立表，分别储存信息
+# 使用selenium+PhantomJS抓取对应博主主页链接
