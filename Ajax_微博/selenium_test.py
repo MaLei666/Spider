9952244b12cf1080d0ea697beb09c8570bb77144
MaLei <826908021@qq.com>
2018-06-04 18:08:27 +0800

    添加cookies访问失败   6.4 18:06 公司更新

--- Ajax_微博/selenium_test.py
+++ Ajax_微博/selenium_test.py
@@ -5,23 +5,39 @@ from pyquery import PyQuery as pq
 from selenium import webdriver
 from time import sleep
 
+
+cookies={}
+# with open('E:\Spider\Ajax_微博\cookie.txt')as file:
+#     raw_cookies=file.read()
+#     for line in raw_cookies.split(';'):
+#         key,value=line.split('=',1)
+#         cookies[key]=value
+#         # print(cookies)
+
+webdriver.DesiredCapabilities.FIREFOX['firefox.page.settings.userAgent'] = "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:21.0) Gecko/20100101 Firefox/21.0"
 browser = webdriver.Firefox()
 browser.get(url='https://m.weibo.cn/')
-cookies={}
-s = requests.session()
-with open('E:\Spider\Ajax_微博\cookie.txt')as file:
-    raw_cookies=file.read()
-    for line in raw_cookies.split(';'):
-        key,value=line.split('=',1)
-        cookies[key]=value
-        # print(cookies)
-    for each in cookies.items():
-        print(type(each))
-        each=eval(each.__str__())
-        browser.add_cookie({'name':each.key(),'value':each[key]})
+sleep(2)
+browser.delete_all_cookies()
+browser.add_cookie({'name': '_T_WM', 'value': '52b0151e9490855bae540e40f881320f', 'path': '/',  'expiry': 1530671807, 'secure': False, 'httpOnly': True})
+browser.add_cookie({'name': 'SUB', 'value': '_2A252ENWfDeRhGeVH6FQX9SzKyD6IHXVV-vvXrDV6PUJbkdANLWHmkW1NT0iViQc71Gn2lMbDYa_ioQFHVnRmZaqg', 'path': '/',  'expiry': 1559615823, 'secure': False, 'httpOnly': True})
+browser.add_cookie({'name': 'SUHB', 'value': '0LcSnbMGfYxAbx', 'path': '/', 'expiry': 1559615823, 'secure': False, 'httpOnly': False})
+browser.add_cookie({'name': 'SCF', 'value': 'AlYBAKMIZxonUEvbmR6JgqyYWHL1yaDreGv8vXPl1FRCO7xm1IoOxBvQbDb-ZqzXTud9qRC3AnLVu7nFx_MvHdc.', 'path': '/',  'expiry': 1843439823, 'secure': False, 'httpOnly': True})
+browser.add_cookie({'name': 'SSOLoginState', 'value': '1528079823', 'path': '/','expiry': None, 'secure': False, 'httpOnly': False})
+browser.add_cookie({'name': 'MLOGIN', 'value': '1', 'path': '/', 'expiry': 1528083425, 'secure': False, 'httpOnly': False})
+browser.add_cookie({'name': 'M_WEIBOCN_PARAMS', 'value': 'uicode%3D20000174%26featurecode%3D20000320%26fid%3Dhotword', 'path': '/', 'expiry': 1528080425, 'secure': False, 'httpOnly': True})
 
-# browser.add_cookie({'name':'M_WEIBOCN_PARAMS','value':'featurecode%3D20000320%26oid%3D4245425282732969%26luicode%3D20000061%26lfid%3D4245425282732969%26uicode%3D20000174%26fid%3Dhotword'})
-browser.refresh()
+sleep(2)
+browser.get(url='https://m.weibo.cn/')
+sleep(40)
+print(browser.get_cookies())
+# elem=browser.find_element_by_class_name('btn btnWhite')
+# elem.click()
+# username=browser.find_element_by_id('loginName')
+    # username.send_keys('18401570769')
+
+
+# browser.refresh()
 # print(browser.page_source)
 # if browser.find_element_by_class_name()
 
