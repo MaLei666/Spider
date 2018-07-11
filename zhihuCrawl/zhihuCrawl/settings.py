ea6578cacc34691e46aafc2b003cf6afe48d9fcb
MaLei <826908021@qq.com>
2018-07-11 18:16:26 +0800

    7.9 更新scrapy-redis组件

--- zhihuCrawl/zhihuCrawl/settings.py
+++ zhihuCrawl/zhihuCrawl/settings.py
@@ -65,8 +65,8 @@ COOKIES_ENABLED = True
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'zhihuCrawl.pipelines.ZhihucrawlPipeline': 300,
-   'scrapy_redis.pipelines.RedisPipeline': 301
+   'zhihuCrawl.pipelines.ZhihucrawlPipeline': 300
+   # 'scrapy_redis.pipelines.RedisPipeline': 301
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
@@ -100,16 +100,16 @@ MONGODB_DBNAME = "python"
 MONGODB_SHEETNAME = "python"
 
 # #使用scrapy_redis的调度器:
-SCHEDULER="scrapy_redis.scheduler.Scheduler"
+# SCHEDULER="scrapy_redis.scheduler.Scheduler"
 
 #在Redis中保持。scrapy-redis用到的各个队列，从而允许暂停和暂停后恢复:
 # SCHEDULER_PERSIST=True
 
 # 使用scrapy_redis的去重方式:
-DUPEFILTER_CLASS="scrapy_redis.dupefilter.RFPDupeFilter"
+# DUPEFILTER_CLASS="scrapy_redis.dupefilter.RFPDupeFilter"
 
 # #定义Redis的ip和端口:
 # REDIS_HOST='127.0.0.1'
 # REDIS_PORT=6379
-REDIS_URL = 'redis://redis:123456@127.0.0.1:6379'
+# REDIS_URL = 'redis://redis:123456@127.0.0.1:6379'
 
