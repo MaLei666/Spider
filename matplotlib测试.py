bb23498ab1a7950d0effe7ea08a0f186bfad1a86
MaLei <826908021@qq.com>
2018-08-01 17:56:57 +0800

    8.1更新

--- matplotlib测试.py
+++ matplotlib测试.py
@@ -146,16 +146,27 @@ from mpl_toolkits.basemap import Basemap
 # plt.title('Basemap Tutorial')
 # plt.show()
 
-from mpl_toolkits.mplot3d import axes3d
+from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import style
-style.use('fivethirtyeight')
-fig=plt.figure()
-ax1=fig.add_subplot(111,projection='3d')
-x = [1,2,3,4,5,6,7,8,9,10]
-y = [5,6,7,8,2,5,6,3,7,2]
-z = [1,2,6,3,2,7,3,3,7,2]
-ax1.plot_wireframe(x,y,z)
+style.use('ggplot')
+
+fig = plt.figure()
+ax1 = fig.add_subplot(111, projection='3d')
+
+x3 = [1,2,3,4,5,6,7,8,9,10]
+y3 = [5,6,7,8,2,5,6,3,7,2]
+z3 = np.zeros(10)
+
+dx = np.ones(10)
+dy = np.ones(10)
+dz = [1,2,3,4,5,6,7,8,9,10]
+
+ax1.bar3d(x3, y3, z3, dx, dy, dz)
+
+
 ax1.set_xlabel('x axis')
 ax1.set_ylabel('y axis')
 ax1.set_zlabel('z axis')
+
 plt.show()
+
