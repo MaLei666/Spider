49e3a914a1468ac99294f05f919464f269ba542f
MaLei <826908021@qq.com>
2018-10-24 11:31:32 +0800

    10.24更新

---
+++ appium_test/app_test.py
@@ -0,0 +1,37 @@
+#!/home/zkfr/.local/share/virtualenvs/xf-5EfV3Nly/bin/python
+#-*- coding:utf-8 -*-
+# @author : MaLei 
+# @datetime : 2018-10-24 10:37
+# @file : app_test.py
+# @software : PyCharm
+
+from appium import webdriver
+
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps['platformVersion'] = '8.0'
+desired_caps['deviceName'] = 'Android Emulator'
+# desired_caps['appPackage'] = 'com.android.calculator2'
+# desired_caps['appActivity'] = '.Calculator'
+
+driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+
+driver.find_element_by_name("1").click()
+
+driver.find_element_by_name("5").click()
+
+driver.find_element_by_name("9").click()
+
+driver.find_element_by_name("delete").click()
+
+driver.find_element_by_name("9").click()
+
+driver.find_element_by_name("5").click()
+
+driver.find_element_by_name("+").click()
+
+driver.find_element_by_name("6").click()
+
+driver.find_element_by_name("=").click()
+
+driver.quit()
