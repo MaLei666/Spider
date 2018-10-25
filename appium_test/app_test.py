52cfc98b8f0ccca0be37a40b71260cb92ad40c31
MaLei <826908021@qq.com>
2018-10-25 18:02:29 +0800

    10.25更新

--- appium_test/app_test.py
+++ appium_test/app_test.py
@@ -5,33 +5,25 @@
 # @file : app_test.py
 # @software : PyCharm
 
+import os
+import time
+import unittest
+from selenium import webdriver
+from lib2to3.pgen2.driver import Driver
+# from lib2to3.tests.support import driver
 from appium import webdriver
 
-desired_caps = {}
-desired_caps['platformName'] = 'Android'
-desired_caps['platformVersion'] = '8.0'
-desired_caps['deviceName'] = 'Android Emulator'
-# desired_caps['appPackage'] = 'com.android.calculator2'
-# desired_caps['appActivity'] = '.Calculator'
+PATH=lambda p:os.path.abspath(os.path.join(os.path.dirname(__file__),p))
 
+desired_caps = {}
+desired_caps['platformName'] = 'Android'  #设备系统
+desired_caps['platformVersion'] = '8.0.0'  #设备系统版本
+desired_caps['deviceName'] = 'Galaxy S8'  #设备名称
+# desired_caps['app'] = PATH('D:\\下载\\jumeilatest.apk')
+desired_caps['appPackage'] = 'com.jm.android.jumei'
+desired_caps['appActivity'] = 'com.jm.android.jumei.home.activity.StartActivity'
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
-driver.find_element_by_name("1").click()
+# driver.launch_app()
+time.sleep(10)
 
-driver.find_element_by_name("5").click()
-
-driver.find_element_by_name("9").click()
-
-driver.find_element_by_name("delete").click()
-
-driver.find_element_by_name("9").click()
-
-driver.find_element_by_name("5").click()
-
-driver.find_element_by_name("+").click()
-
-driver.find_element_by_name("6").click()
-
-driver.find_element_by_name("=").click()
-
-driver.quit()
