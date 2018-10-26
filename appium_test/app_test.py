834cb83b3f8226bd55d6e0c1f339054875be857e
MaLei <826908021@qq.com>
2018-10-26 17:42:07 +0800

    10.26更新 appium模拟操作真机

--- appium_test/app_test.py
+++ appium_test/app_test.py
@@ -5,15 +5,8 @@
 # @file : app_test.py
 # @software : PyCharm
 
-import os
 import time
-import unittest
-from selenium import webdriver
-from lib2to3.pgen2.driver import Driver
-# from lib2to3.tests.support import driver
 from appium import webdriver
-
-PATH=lambda p:os.path.abspath(os.path.join(os.path.dirname(__file__),p))
 
 desired_caps = {}
 desired_caps['platformName'] = 'Android'  #设备系统
@@ -22,8 +15,34 @@ desired_caps['deviceName'] = 'Galaxy S8'  #设备名称
 # desired_caps['app'] = PATH('D:\\下载\\jumeilatest.apk')
 desired_caps['appPackage'] = 'com.jm.android.jumei'
 desired_caps['appActivity'] = 'com.jm.android.jumei.home.activity.StartActivity'
+
+
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-
+time.sleep(6)
+x=driver.get_window_size()['width']
+y=driver.get_window_size()['height']# 左滑
+driver.swipe(x*0.75,y*0.5,x*0.25,y*0.5,100)
+time.sleep(2)
+# 上滑
+driver.swipe(x*0.5,y*0.75,x*0.5,y*0.2,100)
+print('手动点击登录')
+time.sleep(6)
+print('使用微信登录')
+driver.tap([(159,1890),(349,1986)],100)
+# 手动关掉弹窗
+time.sleep(2)
 # driver.launch_app()
-time.sleep(10)
+for i in range(0,10):
+    time.sleep(17)
+    driver.swipe(x*0.5,y*0.75,x*0.5,y*0.25,200)
 
+# 收起键盘
+driver.hide_keyboard()
+# 查看当前activity，只适用于Android
+driver.current_activity()
+# 设备中拉出文件
+driver.pull_file('/path')
+# 推送文件
+data='test'
+driver.push_file('/path',data.encode('base64'))
+driver.shake()
